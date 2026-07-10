88a16024d495c68d9368c61e0cf0e2a56af75b70
dwaryarr <dwi.aryaramadhni@gmail.com>
2026-07-11 01:56:09 +0700

    Updates

--- assets/arrow-right-C4EOyZhQ.js
+++
@@ -1,6 +0,0 @@
-import{c as r}from"./index-CO_lKZsh.js";/**
- * @license lucide-react v0.469.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=r("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]);export{o as A};
